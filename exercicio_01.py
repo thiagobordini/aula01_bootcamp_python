723b29c7f3ecbd1fc71ef9799e6c7e3289370c01
thiagobordini <thiagobordini@outlook.com>
2025-09-13 17:21:31 -0300

    Exercício 01 - Bootcamp Python

---
+++ exercicio_01.py
@@ -0,0 +1,7 @@
+# 1) Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+# Recebendo o nome do usuário e armazenando o conteúdo na variável "nome_usuario"
+nome_usuario = input("Digite o seu nome: ")
+
+# Printando uma mensagem de boas vindas utilizando f-string
+print(f"Seja bem-vindo(a), {nome_usuario}!")
