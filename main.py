f8963e3342897b6ed08738389d8191ff8c7cead3
thiagobordini <thiagobordini@outlook.com>
2025-09-13 17:20:29 -0300

    Aula 01 - Bootcamp Python

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+# Aula 01 - Bootacamp Python (Jornada de Dados)
+
+# 1) Método print
+# Impressão do texto "Hello, World!" com quebra de linha
+print("Hello, World!\n")
+
+# 2) Método input
+# Recebendo e printando o nome do usuário com f-string
+print(f"Olá, {input("Qual seu nome? ")}!")
+
+# 3) Variáveis e tipos de dados
+# Criando e armazenando diferentes tipos de dados em variáveis
+nome = "Thiago"  # string
+inteiro = 19  # int
+decimal = 9.5  # float
+booleano = True  # Booleano (True ou False)
